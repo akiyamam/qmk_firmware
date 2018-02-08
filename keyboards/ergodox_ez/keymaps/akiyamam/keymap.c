72f7f510154673c63530ce5703c7fac560479c5d
akiyamam <36131478+akiyamam@users.noreply.github.com>
2018-02-09 01:16:52 +0900

    Update

--- keyboards/ergodox_ez/keymaps/akiyamam/keymap.c
+++ keyboards/ergodox_ez/keymaps/akiyamam/keymap.c
@@ -89,7 +89,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,   KC_Q,        KC_W,    KC_E,    KC_R,    KC_T,  TG(SYMB),
         KC_LCTRL, KC_A,        KC_S,    KC_D,    KC_F,    KC_G,
         KC_LSFT,  CTL_T(KC_Z), KC_X,    KC_C,    KC_V,    KC_B,  ALL_T(KC_NO),
-        KC_LEFT,  KC_LALT,     KC_LALT, KC_LGUI, MT(KC_LALT, KC_MHEN),
+        KC_LEFT,  KC_LALT,     KC_LALT, KC_LGUI, MT(KC_RALT, KC_MHEN),
                                               ALT_T(KC_APP),   KC_LGUI,
                                                                KC_HOME,
                                               KC_SPC,MO(SYMB), KC_END,
