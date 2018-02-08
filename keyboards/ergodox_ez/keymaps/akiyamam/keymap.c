67efd55d4167d4a33f0acc87e843fd2fa4341da8
akiyamam <36131478+akiyamam@users.noreply.github.com>
2018-02-08 20:45:53 +0900

    Update Windows keymap

--- keyboards/ergodox_ez/keymaps/akiyamam/keymap.c
+++ keyboards/ergodox_ez/keymaps/akiyamam/keymap.c
@@ -4,9 +4,10 @@
 #include "version.h"
 
 #define BASE 0 // default layer
-#define SYMB 1 // symbols
-#define MDIA 2 // media keys
-#define WIN  3 // windows keys
+#define WIN  1 // windows keys
+#define SYMB 2 // symbols
+#define MDIA 3 // media keys
+
 
 enum custom_keycodes {
   PLACEHOLDER = SAFE_RANGE, // can always be here
@@ -53,13 +54,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
        	     KC_6,         KC_7,   KC_8,    KC_9,   KC_0,    KC_MINS,           KC_BSPC,  
              TG(SYMB),     KC_Y,   KC_U,    KC_I,   KC_O,    KC_P,              KC_BSLS,
                            KC_H,   KC_J,    KC_K,   KC_L,    LT(MDIA, KC_SCLN), KC_ENT,
-             MEH_T(KC_NO), KC_N,   KC_M,    KC_COMM,KC_DOT,  KC_SLSH,           KC_RSFT,
+	     DF(WIN), KC_N,   KC_M,    KC_COMM,KC_DOT,  KC_SLSH,           KC_RSFT,
                                    KC_RGUI, KC_RALT,KC_LEFT, KC_DOWN,           KC_RIGHT,
              KC_LALT,          KC_ESC,
              KC_PGUP,
 	KC_PGDN, KC_SPACE , MO(SYMB)
     ),
-/* Keymap 0: Basic layer
+/* Keymap 1: WIN layer
  *
  * ,--------------------------------------------------.           ,--------------------------------------------------.
  * | `      |   1  |   2  |   3  |   4  |   5  |  6   |           |   6  |   7  |   8  |   9  |   0  |   -  |  BSPC  |
@@ -67,10 +68,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * | TAB    |   Q  |   W  |   E  |   R  |   T  |  L1  |           |  L1  |   Y  |   U  |   I  |   O  |   P  |  BSLS  |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
  * | LCTRL  |   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |; / L2|  Enter |
- * |--------+------+------+------+------+------| Hyper|           | Meh  |------+------+------+------+------+--------|
+ * |--------+------+------+------+------+------| Hyper|         |DF(BASE)|------+------+------+------+------+--------|
  * | LShift |Z/Ctrl|   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |   /  | RShift |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *   | LEFT | LALT |AltShf| LAlt | LGUI |                                       | RGUI | RAlt | LEFT | DOWN | RIGHT |
+ *   | LEFT | LALT |AltShf| LGUI | LAlt |                                       | RAlt | RGUI | LEFT | DOWN | RIGHT |
  *   `----------------------------------'                                       `-----------------------------------'
  *                                        ,-------------.       ,-------------.
  *                                        | App  | LGui |       | Alt  |Ctrl/Esc|
@@ -82,13 +83,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 // If it accepts an argument (i.e, is a function), it doesn't need KC_.
 // Otherwise, it needs KC_*
-[WIN] = LAYOUT_ergodox(  // layer 0 : default
+[WIN] = LAYOUT_ergodox(  // layer 3 : WIN
         // left hand
         KC_GRV,   KC_1,        KC_2,    KC_3,    KC_4,    KC_5,  KC_6,
         KC_TAB,   KC_Q,        KC_W,    KC_E,    KC_R,    KC_T,  TG(SYMB),
         KC_LCTRL, KC_A,        KC_S,    KC_D,    KC_F,    KC_G,
         KC_LSFT,  CTL_T(KC_Z), KC_X,    KC_C,    KC_V,    KC_B,  ALL_T(KC_NO),
-        KC_LEFT,  KC_LALT,     KC_LALT, KC_LALT, KC_LGUI,
+        KC_LEFT,  KC_LALT,     KC_LALT, KC_LGUI, KC_LALT,
                                               ALT_T(KC_APP),   KC_LGUI,
                                                                KC_HOME,
                                               KC_SPC,MO(SYMB), KC_END,
@@ -96,8 +97,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
        	     KC_6,         KC_7,   KC_8,    KC_9,   KC_0,    KC_MINS,           KC_BSPC,  
              TG(SYMB),     KC_Y,   KC_U,    KC_I,   KC_O,    KC_P,              KC_BSLS,
                            KC_H,   KC_J,    KC_K,   KC_L,    LT(MDIA, KC_SCLN), KC_ENT,
-             MEH_T(KC_NO), KC_N,   KC_M,    KC_COMM,KC_DOT,  KC_SLSH,           KC_RSFT,
-                                   KC_RGUI, KC_RALT,KC_LEFT, KC_DOWN,           KC_RIGHT,
+	     DF(BASE), KC_N,   KC_M,    KC_COMM,KC_DOT,  KC_SLSH,           KC_RSFT,
+                                   KC_RALT, KC_RGUI,KC_LEFT, KC_DOWN,           KC_RIGHT, 
              KC_LALT,          KC_ESC,
              KC_PGUP,
 	KC_PGDN, KC_SPACE , MO(SYMB)
