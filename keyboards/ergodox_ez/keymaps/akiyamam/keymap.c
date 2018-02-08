02b51aa1d6b4786b104b5a80c9cf1343ec6ef253
akiyamam <36131478+akiyamam@users.noreply.github.com>
2018-02-09 01:13:44 +0900

    Update

--- keyboards/ergodox_ez/keymaps/akiyamam/keymap.c
+++ keyboards/ergodox_ez/keymaps/akiyamam/keymap.c
@@ -89,7 +89,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,   KC_Q,        KC_W,    KC_E,    KC_R,    KC_T,  TG(SYMB),
         KC_LCTRL, KC_A,        KC_S,    KC_D,    KC_F,    KC_G,
         KC_LSFT,  CTL_T(KC_Z), KC_X,    KC_C,    KC_V,    KC_B,  ALL_T(KC_NO),
-        KC_LEFT,  KC_LALT,     KC_LALT, KC_LGUI, KC_LALT,
+        KC_LEFT,  KC_LALT,     KC_LALT, KC_LGUI, MT(KC_LALT, KC_MHEN),
                                               ALT_T(KC_APP),   KC_LGUI,
                                                                KC_HOME,
                                               KC_SPC,MO(SYMB), KC_END,
@@ -98,7 +98,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
              TG(SYMB),     KC_Y,   KC_U,    KC_I,   KC_O,    KC_P,              KC_BSLS,
                            KC_H,   KC_J,    KC_K,   KC_L,    LT(MDIA, KC_SCLN), KC_ENT,
 	     DF(BASE), KC_N,   KC_M,    KC_COMM,KC_DOT,  KC_SLSH,           KC_RSFT,
-                                   KC_RALT, KC_RGUI,KC_LEFT, KC_DOWN,           KC_RIGHT, 
+                                   MT(KC_RALT, KC_HENK), KC_RGUI,KC_LEFT, KC_DOWN,           KC_RIGHT, 
              KC_LALT,          KC_ESC,
              KC_PGUP,
 	KC_PGDN, KC_SPACE , MO(SYMB)
